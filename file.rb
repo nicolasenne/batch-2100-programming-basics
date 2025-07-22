b117a195f2f12f7990d503c3a0a1d36751f8c42b
Nicolas Enne <40183143+nicolasenne@users.noreply.github.com>
2025-07-22 10:53:32 -0300

    Add files via upload

---
+++ file.rb
@@ -0,0 +1,119 @@
+require 'date'
+
+# motivation = 5
+
+# puts "Le Wagon" * motivation
+# puts "Nicolas" * motivation
+
+# puts "Le Wagon".class
+# puts 12.class
+# puts 3.14.class
+# puts ["Le Wagon", 12, 3.14].class
+# puts true.class # boolean
+# puts false.class # boolean
+# puts (1..100).class
+# puts "12".class
+# puts "3.14".class
+
+# puts "batch 2100".upcase
+# puts "LE WAGON".downcase
+
+# greeting = "Hello!"
+# puts greeting.upcase
+
+# p "Hello" 
+# p 'Hello'
+
+# puts "Roberto is #{40 + 1} years old"
+
+# p "12".class
+# p "12".to_i.class
+# p "Nicolas".to_i.class
+
+# puts 1 + 2
+# puts 5 - 3
+# puts 5 * 10
+# puts 20 / 2
+
+# puts 10.even?
+# puts 10.odd?
+
+# p 1984.to_s.class
+
+# p 3.to_f.class
+# p 3.14167.round
+
+# puts ["Deia", "Roberto", "Nicolas"].size
+# puts ["Deia", "Roberto", "Nicolas"].length
+
+# p ["Deia", "Roberto", "Nicolas", "Chloé"].sort
+
+# p ["Deia", "Roberto", "Nicolas", "Chloé"]
+# p %w[Deia Roberto Nicolas Chloé]
+
+# p (1..10).to_a
+# p (1...10).to_a
+
+# greeting = "Hello"
+# age = 18
+
+# puts greeting.upcase
+# puts age.even?
+# puts pi.round
+
+# puts "Deia is #{age + 1} years old"
+
+# city = "Belíssimo Horizonte"
+# city_details = "#{city}, Minas Gerais"
+# puts city_details
+
+# pi = 3.14
+# puts "Pi is equal to #{pi + 3}"
+# puts "Pi is equal to #{pi + 2}"
+
+# puts "Hello John!"
+# puts "Hello Paul!"
+# puts "Hello Ringo!"
+
+# def replace(name, age)
+#   return "Hello #{name.capitalize}. Your age is #{age}."
+# end
+
+# puts replace("roberto", 40)
+# puts replace("nicolas", 20)
+# puts replace("deia", 18)
+
+# def max(x, y)
+#   if x > y
+#     return "#{x} is higher than #{y}"
+#   else
+#     return "#{y} is higher than #{x}"
+#   end
+# end
+
+# puts max(2, 3)
+# puts max(200000128712, 9910192882)
+
+# def today
+#   date = Date.today
+#   date.strftime("%B %d")
+# end
+
+# puts today
+
+# puts "12".to_i
+# total_age = 20
+
+# def replace_name(name)
+  
+# end
+
+# motivation = 5
+
+# puts "Le Wagon " * motivation
+# puts "Nicolas " * motivation
+
+text = 'Hello'
+text.upcase!
+
+puts text
